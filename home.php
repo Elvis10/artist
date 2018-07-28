e0acacabe161887185df9219805ea75cd204d91e
Elvis10 <chepre10@gmail.com>
2018-07-28 14:43:10 -0300

    Archivos

---
+++ home.php
@@ -0,0 +1,11 @@
+<head>
+	<h1>BLOCKCHAIN</h1>
+	<h4>Ejemplo de prueba</h4>
+</head>
+<body>
+	<h4>Gracias por jugar con nosotros</h4>
+	<?php
+		$ i = `python test.py`;
+    	echo $ i;
+	?>
+</body>
